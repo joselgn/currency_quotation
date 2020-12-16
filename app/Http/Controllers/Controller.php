fd34f0b47956c423fa9e889d5dce54cb3813ebe7
joselgn <joselgn@gmail.com>
2020-12-16 13:05:22 -0300

    Adicionando funções de conversão e validação no controller principal para utilizar em outros controllers quando for extendido

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,35 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function _bc_validaCampos($arrParams, $arrRules,  $arrMessages)
+    {
+        //Instancia o validador atraves do helper
+        $validator = \validator($arrParams,$arrRules,$arrMessages);//monta validador
+
+        return $validator;
+    }//_validaCampos
+
+    //Retorna data formatada conforme o padrao passado como string
+    //Y = ano - 4 caracteres
+    //m = mes - 2 caracteres
+    //d = dia - 2 caracteres
+    public function _bc_convertDateFormat($date, $formatOut='d/m/Y H:i:s', $useTime=true, $separateDateFromTime=' ')
+    {
+        $formatIn = 'Y-m-d H:i:s';
+        $formatOut = is_null($formatOut) ?  'd/m/Y H:i:s' : $formatOut;
+        $useTime = is_null($useTime) ? true : $useTime;
+        $separateDateFromTime = is_null($separateDateFromTime) ? ' ' : $separateDateFromTime;
+
+        $dateTreat = $useTime ? $date : $date.' 00:00:00';
+        $dateIn = date($formatIn, strtotime($dateTreat));
+        $dateOut = date($formatOut, strtotime($dateIn));
+
+        if ($useTime)
+            $newDate = str_replace(' ', $separateDateFromTime, $dateOut);
+        else
+            [$newDate] = explode(' ', $dateOut);
+
+        return $newDate;
+    }// _bc_convertDateFormat
 }
